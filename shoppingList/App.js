6fd4f36a4af3585284c10fa6ed67bf1b8ffad3e6
Thaddeus Kalinowski <thad2050306@gmail.com>
2024-01-18 17:54:46 -0500

    shopping list style updated

--- shoppingList/App.js
+++ shoppingList/App.js
@@ -5,21 +5,21 @@ export default function App() {
   return (
     <>
     {/* Set status bar styling */}
-    <StatusBar style="light"/>
+    <StatusBar style="auto"/>
 
     {/* Set safe area view screen container */}
     <SafeAreaView style={styles.appContainer}>
         {/* Shopping list title container */}
         <View style={styles.titleContainer}>
-          <Text style={styles.title}>Shopping List.</Text>
+          <Text style={styles.title}>Shopping List</Text>
         </View>
         {/* Button container */}
         <View style={styles.buttonContainer}>
-          <Text style={styles.text}>Add Item Button goes here</Text>
+          <Text style={styles.buttonText}>Add Item</Text>
         </View>
         {/* Items to get title container */}
         <View style={styles.subtitleContainer}>
-          <Text style={styles.subtitles}>Items To Get</Text>
+          <Text style={styles.subtitles}>Items</Text>
         </View>
           {/* Set list of items container */}
         <View style={styles.listContainer}>
@@ -33,45 +33,51 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    backgroundColor: '#1e085a',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 40
+    paddingTop: 20
   },
   titleContainer: {
     flex: 1,
-    margin: 10,
+    margin: 20,
     paddingHorizontal: 20,
     justifyContent: "center",
-    backgroundColor: "white",
-    borderBottomRightRadius: 20,
-    borderTopLeftRadius: 20
+    backgroundColor: "white"
   },
   title: {
-    fontSize: 40,
-    color: "#5e08cc"
+    fontSize: 48,
+    color: "#000",
+    fontWeight: "bold"
   },
   buttonContainer: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "white"
+    flex: 0.7,
+    marginVertical: 30,
+    paddingHorizontal: 60,
+    backgroundColor: "orange",
+    justifyContent: "center"
+  },
+  buttonText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white"
   },
   subtitleContainer: {
     flex: 1,
     margin: 10,
-    paddingHorizontal: 20,
+    paddingHorizontal: 130,
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
     justifyContent: "center",
     backgroundColor: "white",
-    borderBottomRightRadius: 20,
-    borderTopLeftRadius: 20
   },
-  subtitle: {
-    fontSize: 30,
-    color: "#5e08cc"
+  subtitles: {
+    fontSize: 24
   },
   listContainer: {
     flex: 5,
-    backgroundColor: "#123456",
+    paddingHorizontal: 80,
+    backgroundColor: "white",
     justifyContent: "center"
   }
 });
