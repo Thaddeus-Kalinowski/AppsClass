f7937234badc40cb8ce8d5c19d7381ac30f00eca
Thaddeus Kalinowski <thad2050306@gmail.com>
2024-02-21 20:48:09 -0500

    start BikeShop app

---
+++ Week 7/BikeShop/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+
+  let screen = <></>
+
+  return (
+    <>
+      <StatusBar style="auto" />
+      <SafeAreaProvider>{screen}</SafeAreaProvider>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
